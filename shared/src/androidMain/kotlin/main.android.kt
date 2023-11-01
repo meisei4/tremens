b9c69c459ead6ce52f5a4be2b9fa8a7868c8c01b
meisei4 <lanur622@gmail.com>
2023-11-01 09:39:00 +0900

    fixed build remaining errors with sqldelight integration

--- shared/src/androidMain/kotlin/main.android.kt
+++ shared/src/androidMain/kotlin/main.android.kt
@@ -2,4 +2,4 @@ import androidx.compose.runtime.Composable
 
 actual fun getPlatformName(): String = "Android"
 
-@Composable fun MainView() = AppView(viewModel = MainViewModel(model = MainModel()))
+@Composable fun MainView() = AppView(viewModel = MainViewModel(model = MainModel(habitStorage = MainHabitStorage())))
---
+++ shared/src/commonMain/kotlin/MainHabitStorage.kt
@@ -0,0 +1,4 @@
+class MainHabitStorage(): HabitStorage() {
+
+
+}
