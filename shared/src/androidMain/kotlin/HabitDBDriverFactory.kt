37eb85abb02622cd439fe54b654e9f7dd2f76a4a
meisei4 <lanur622@gmail.com>
2023-11-01 12:24:13 +0900

    forgot to version androidMain factory

---
+++ shared/src/androidMain/kotlin/HabitDBDriverFactory.kt
@@ -0,0 +1,10 @@
+import android.content.Context
+import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.driver.android.AndroidSqliteDriver
+import tremens.database.HabitDatabase
+
+actual class HabitDBDriverFactory(private val context: Context) {
+    actual fun createDriver(): SqlDriver {
+        return AndroidSqliteDriver(HabitDatabase.Schema, context, "test.db")
+    }
+}
--- shared/src/androidMain/kotlin/main.android.kt
+++ shared/src/androidMain/kotlin/main.android.kt
@@ -1,7 +1,13 @@
 import androidx.compose.runtime.Composable
 import android.content.Context
+import tremens.database.HabitDatabase
 
 actual fun getPlatformName(): String = "Android"
 
 @Composable fun MainView() = AppView(viewModel = MainViewModel(model = MainModel(habitStorage = MainHabitStorage(
-    database = HabitDBDriverFactory(context = Context).createDriver()))))
+    database = HabitDatabase(HabitDBDriverFactory().createDriver())
+))))
+
+fun HabitDBDriverFactory(): HabitDBDriverFactory {
+
+}
