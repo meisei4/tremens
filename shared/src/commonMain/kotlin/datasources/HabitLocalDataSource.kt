e034373fef857dfab9823237477fb28fffa802de
meisei4 <lanur622@gmail.com>
2023-11-04 16:27:37 +0900

    Fixed the uniqueness contraints on the sqldelight db.

--- shared/src/commonMain/kotlin/datasources/HabitLocalDataSource.kt
+++ shared/src/commonMain/kotlin/datasources/HabitLocalDataSource.kt
@@ -4,7 +4,9 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import kotlinx.datetime.Clock
 import kotlinx.datetime.DateTimeUnit
+import kotlinx.datetime.LocalDate
 import kotlinx.datetime.TimeZone
+import kotlinx.datetime.atStartOfDayIn
 import kotlinx.datetime.minus
 import kotlinx.datetime.toInstant
 import kotlinx.datetime.toLocalDateTime
@@ -28,17 +30,19 @@ class HabitLocalDataSource(database: HabitDatabase): HabitDataSource {
         val dateToStatusMap = lastFiveDates.zip(habit.lastFiveDayStatuses).toMap()
         val datesToInsert = dateToStatusMap.filter { it.value == true }.keys
         datesToInsert.forEach { dateString ->
-            val localDateTime = dateString.toLocalDateTime()
-            val unixTimestamp = localDateTime.toInstant(TIME_ZONE).epochSeconds
+            val localDate = LocalDate.parse(dateString) // Use LocalDate.parse here
+            val instant = localDate.atStartOfDayIn(TIME_ZONE) // Convert LocalDate to Instant at the start of the day
+            val unixTimestamp = instant.epochSeconds
             trackingQueries.insertTracking(habitId, unixTimestamp)
         }
     }
 
     override suspend fun getAllHabits(): List<HabitRowData> = withContext(Dispatchers.Unconfined) {
         val lastFiveDates = getLastFiveDates()
-        TimeZone.currentSystemDefault()
-
-        val lastFiveDatesTimestamps = lastFiveDates.map { it.toLocalDateTime().toInstant(TIME_ZONE).epochSeconds }
+        val lastFiveDatesInstants = lastFiveDates.map {
+            LocalDate.parse(it).atStartOfDayIn(TIME_ZONE)
+        }
+        val lastFiveDatesTimestamps = lastFiveDatesInstants.map { it.epochSeconds }
         val habitNames = habitQueries.selectAllHabitNames().executeAsList()
 
         //this is kind of crazy but i think its so far the clearest way without doing the antipattern
@@ -62,12 +66,12 @@ class HabitLocalDataSource(database: HabitDatabase): HabitDataSource {
         return@withContext habitRowDataList
     }
 
-
     //TODO figure out this suspend issue and Coroutines and context
     override fun updateTracking(habitName: String, updatedTracking: List<Boolean>) {//} = withContext(Dispatchers.Unconfined) {
         val habitId = habitQueries.getHabitId(habitName).executeAsOne()
         val lastFiveDates = getLastFiveDates()
-        val lastFiveDatesTimestamps = lastFiveDates.map { it.toLocalDateTime().toInstant(TIME_ZONE).epochSeconds }
+        val lastFiveDatesInstants = lastFiveDates.map { LocalDate.parse(it).atStartOfDayIn(TIME_ZONE) } // Parse to LocalDate and get Instant
+        val lastFiveDatesTimestamps = lastFiveDatesInstants.map { it.epochSeconds }
 
         val dateToStatusMap = lastFiveDatesTimestamps.zip(updatedTracking).toMap()
 
